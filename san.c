ab42e4c7a965b2e5c302ba3ff116948fa389b7f2
sandhyaaruna <35653085+sandhyaaruna@users.noreply.github.com>
2018-02-09 16:51:08 +0530

    Create san.c

---
+++ san.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a[10],i,n,max=-999;
+clrscr();
+scanf("%d",&n);
+for(i=0;i<n;i++)
+{
+sacnf("%d",&a[i]);
+{
+if(a[i]>max)
+{
+max=a[i];
+}
+printf("%d",max);
+getch();
+}
+
