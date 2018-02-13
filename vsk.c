2d54f5245bab368c2e7c80f1f5c6eb4c1cdd1296
sandhyaaruna <35653085+sandhyaaruna@users.noreply.github.com>
2018-02-13 16:55:05 +0530

    Create vsk.c

---
+++ vsk.c
@@ -0,0 +1,25 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+int i=0,f=0,n=0;
+char str[100];
+clrscr();
+printf("enter the string value");
+gets(str);
+for(i=0;i(str)!=0;i++)
+{
+n=n+1;
+}
+printf("the number of character in the string %d\n",n);
+for (i=0;i<=n-1;i++)
+{
+if (str[i]==);
+{
+f=f+1;
+}
+}
+printf("the numberof words in the string are %d",f);
+getch();
+}
