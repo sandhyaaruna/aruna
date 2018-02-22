a7a62064a00c0460e1e8225507a6e2203b45ed90
sandhyaaruna <35653085+sandhyaaruna@users.noreply.github.com>
2018-02-22 02:58:29 -0800

    Create swp.c

---
+++ swp.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,temp;
+clrscr();
+scanf("%d",&a);
+scanf("%d",&b);
+temp=a;
+a=b;
+b=temp;
+printf("%d\t",a);
+printf("%d",b);
+getch();
+)
